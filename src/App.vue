d781461209c5f922ff43cc26b73cbdcba810d16d
RafaLevina06 <brafalevina06@gmail.com>
2024-02-20 21:50:15 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h1>My-Todo-s</h1>
+    <div id="app">
+      <TodoBoard :todos="todos" @addTodo="addTodo" @deleteTodo="deleteTodo" @changeStatusTodo="changeStatusTodo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoBoard from './components/TodoBoard.vue';
+
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push(newTodo);
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
+    },
+    changeStatusTodo(todoId) {
+      this.todos = this.todos.map(todo => {
+        if (todo.id === todoId) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+    },
+  },
+  components: {
+    TodoBoard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+.container {
+  display: block;
+  background-color: grey;
+  width: 800px;
+  height: 300px;
+  box-shadow: transparent;
+}
+</style>
